2b1cde3b22c92cfb67cd8baaacb73bc157a1e4e2
Felipe Augusto Felix <felixti@live.com>
2019-04-24 17:59:44 -0300

    feat: adding Search book container
    adding Search book container files

---
+++ src/containers/SearchBook/search-book.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./search-book.style.css";
+import Book from "../../components/Book/book";
+
+const SearchBook = props => {
+  const { handleBookSearch, books, onChangeBookShelf } = props;
+
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            onChange={handleBookSearch}
+            placeholder="Search by title or author"
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {books.map((b, index) => (
+            <li key={`${b.shelf}-${index}`}>
+              <Book
+                key={`${b.title}-${index}`}
+                bookId={b.id}
+                bookTitle={b.title}
+                bookAuthor={b.authors ? b.authors[0] : ""}
+                thumbnail={b.imageLinks ? b.imageLinks.smallThumbnail : ""}
+                defaultShelf={b.shelf}
+                onChangeBookShelf={onChangeBookShelf}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+SearchBook.propTypes = {
+  handleBookSearch: PropTypes.func.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired,
+  books: PropTypes.array
+};
+
+export default SearchBook;
